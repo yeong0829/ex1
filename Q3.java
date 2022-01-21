930b24b7f4b604704e35104c5f6acbea56ad6718
ㅈㅅㅇ <w2017@e-mirim.hs.kr>
2022-01-21 19:31:12 +0900

    문제3
    문제3

---
+++ Q3.java
@@ -0,0 +1,31 @@
+package test;
+
+public class Q3 {
+
+	public static void main(String[] args) {
+		Calculator2 c = new Calculator2();
+		System.out.println(c.isOdd(3));
+	}
+
+}
+
+class Calculator2{
+	int v;
+
+	public Calculator2() {
+		this.v = 0;
+	}
+	
+	void add(int n) {
+		this.v += n;
+	}
+	
+	int getValue() {
+		return this.v;
+	}
+	
+	boolean isOdd(int n) {
+		 return n % 2 == 1;
+	}
+	
+}
