7d092011b1a561c2e5094cc5e771ffaf0488cdf7
ㅈㅅㅇ <w2017@e-mirim.hs.kr>
2022-01-25 08:22:44 +0900

    문제6
    문제6

---
+++ Q6.java
@@ -0,0 +1,27 @@
+package test;
+
+public class Q6 {
+
+	public static void main(String[] args) {
+		 Calculator4 c = new Calculator4();
+	     c.add(3);  
+	     System.out.println(c.getValue());
+	}
+
+}
+
+class Calculator4 {
+    int v;
+
+    Calculator4() {
+        this.v = 0;
+    }
+
+    void add(int n) {
+        this.v += n;
+    }
+
+    public Integer getValue() {
+        return this.v;
+    }
+}
