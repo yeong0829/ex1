9498b3d9e2efde937e5c7a224d425ea37312eeae
ㅈㅅㅇ <w2017@e-mirim.hs.kr>
2022-02-26 14:59:27 +0900

    Update Q9.java

--- Q9.java
+++ Q9.java
@@ -7,10 +7,10 @@ public class Q9 {
 
 	public static void main(String[] args)throws IOException {
 		FileWriter fw = new FileWriter("sample.txt");
-        System.out.print("¹®Àå ÀÔ·Â:");
-        Scanner sc = new Scanner(System.in);
-        String line = sc.nextLine();
-        fw.write(String.format("%s\n", line));
-        fw.close();
+       		System.out.print("¹®Àå ÀÔ·Â:");
+       		Scanner sc = new Scanner(System.in);
+        	String line = sc.nextLine();
+        	fw.write(String.format("%s\n", line));
+        	fw.close();
 	}
 }
